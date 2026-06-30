b530db625d052682cc23c5586ee3057aaa1c7f05
SrBonilla2003 <jarod.bonilla.granados@est.una.ac.cr>
2026-06-29 19:33:48 -0600

    forgotten change of security making mistakes on production

--- Backend/src/main/java/com/jarod/fintrack/config/SecurityConfig.java
+++ Backend/src/main/java/com/jarod/fintrack/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -46,11 +45,6 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager() {
         return new org.springframework.security.authentication.ProviderManager(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().requestMatchers("/api/auth/**");
     }
 
     @Bean
